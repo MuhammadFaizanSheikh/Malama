79da4694cfcf51bf6d19fa74b27cb645bda98d3d
Muhammad Faizan <muhamadfaizansheikh@gmail.com>
2024-11-08 00:14:51 +0500

    restrict inactive user to login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,43 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ExcelFilesCompiler.Controllers
 {
-    //public class AccountController : Controller
-    //{
-    //    [HttpGet]
-    //    public IActionResult Login()
-    //    {
-    //        return View();
-    //    }
-
-    //    [HttpPost]
-    //    public IActionResult Login(LoginViewModel model)
-    //    {
-    //        // Hardcoded credentials
-    //        string hardcodedUsername = "DAWSON";
-    //        string hardcodedPassword = "123456";
-
-    //        if (model.Username != null && model.Password != null)
-    //        {
-    //            if (model.Username.ToUpper() == hardcodedUsername && model.Password == hardcodedPassword)
-    //            {
-    //                // Redirect to Home page if login is successful
-    //                return RedirectToAction("Index", "Dashboard");
-    //            }
-    //            else
-    //            {
-    //                // Return an error message if login fails
-    //                ViewBag.ErrorMessage = "Invalid username or password";
-    //                return View();
-    //            }
-    //        }
-    //        else
-    //        {
-    //            // Return an error message if login fails
-    //            ViewBag.ErrorMessage = "Invalid username or password";
-    //            return View();
-    //        }
-    //    }
-    //}
     public class AccountController : Controller
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
@@ -85,6 +48,12 @@ namespace ExcelFilesCompiler.Controllers
                     if (user == null)
                     {
                         ModelState.AddModelError("", "User not found.");
+                        return View(model);
+                    }
+
+                    if (user.IsActive == false)
+                    {
+                        ModelState.AddModelError("", "User is inactive.");
                         return View(model);
                     }
 
