de80e9cda1764825b60fa62dbb931617d8b21dc0
Muhammad Faizan <muhamadfaizansheikh@gmail.com>
2025-04-14 01:22:06 +0500

    update of completion of staff requirement count task

--- Migrations/20250413184449_Test41.Designer.cs
+++ Migrations/20250413184449_Test41.Designer.cs
@@ -4,6 +4,7 @@ using ExcelFilesCompiler;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExcelFilesCompiler.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250413184449_Test41")]
+    partial class Test41
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -811,6 +814,7 @@ namespace ExcelFilesCompiler.Migrations
                         .HasColumnType("bit");
 
                     b.Property<string>("SelectedStation")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
--- Models/EventManagementDto.cs
+++ Models/EventManagementDto.cs
@@ -400,7 +400,7 @@
         public List<DateTime>? AvailabilityDates { get; set; } // List of selected dates
         public List<EventManagementStaffAvailability> AvailabilityDatesList { get; set; } = new List<EventManagementStaffAvailability>();
         public bool PreEventAvailability { get; set; } // Pre-Event checkbox
-        public string SelectedStation { get; set; } // Pre-Event checkbox
+        public string? SelectedStation { get; set; } // Pre-Event checkbox
 
 
     }
