7f529e6a6cd73468d9cdf8582da41a41f3f916b6
Muhammad Faizan <muhamadfaizansheikh@gmail.com>
2025-04-25 00:19:24 +0500

    update of EventServiceDetail IsConfirmed addition task

---
+++ Migrations/20250424185504_Test44.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExcelFilesCompiler.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test44 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfirmed",
+                table: "EventServiceDetail",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfirmed",
+                table: "EventServiceDetail");
+        }
+    }
+}
