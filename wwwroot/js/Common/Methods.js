f742555a5b219e65baa99a3a4141477315f35d9b
Muhammad Faizan <muhamadfaizansheikh@gmail.com>
2025-12-30 01:39:30 +0500

    change date to flatpicker for report processor

--- wwwroot/js/Common/Methods.js
+++ wwwroot/js/Common/Methods.js
@@ -5,6 +5,15 @@
     return dateTimeString.split('T')[0];
 }
 
+function getyyyymmddDateGlobalAgainstDate(date) {
+    if (!date) return "";
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // months 0-11
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`; // YYYY-MM-DD
+}
+
+
 function formatDateToMMDDYYYYGlobal(dateStr) {
     const [y, m, d] = dateStr.split('-');
     return `${m}/${d}/${y}`; // MM/DD/YYYY
